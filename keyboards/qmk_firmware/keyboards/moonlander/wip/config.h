faaf927e6e9d2efdd4512c76b3bfa80a35782219
Lennart Boecken <lsboecken@gmail.com>
2023-10-06 15:11:15 -0400

    add moonlander config

---
+++ keyboards/qmk_firmware/keyboards/moonlander/wip/config.h
@@ -0,0 +1,27 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef RGB_DISABLE_TIMEOUT
+#define RGB_DISABLE_TIMEOUT 300000
+
+#undef RGB_MATRIX_VAL_STEP
+#define RGB_MATRIX_VAL_STEP 24
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 6
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 72
+
+#define NO_AUTO_SHIFT_TAB
+#define FIRMWARE_VERSION u8"d73OX/bPW6Q"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
