a0a22a64a18192def37d8653f77298df97a52d0d
Lennart Boecken <lsboecken@gmail.com>
2023-10-09 13:02:19 -0400

    fix tmux script & move zsh keybinds

---
+++ zsh/.oh-my-zsh/custom/keybinds.zsh
@@ -0,0 +1,18 @@
+alias vs="code ."
+alias nw="~/bin/tmux_newwindow"
+alias ts="~/bin/tmux_sessionizer"
+alias dkcons="~/bin/docker_current_containers"
+alias dkbash="~/bin/docker_run_bash_on_container"
+alias dkid="~/bin/docker_container_id"
+alias ls="ls -a" 
+alias gre="~/bin/git_rebase"
+alias gsm="git switch master"
+alias vim="nvim"
+# Commands to quickly navigate between projects
+alias snap="ts snap"
+alias symserv="ts services"
+alias symapp="ts symapps"
+
+#bitwarden
+alias bw="~/bin/bw"
+
