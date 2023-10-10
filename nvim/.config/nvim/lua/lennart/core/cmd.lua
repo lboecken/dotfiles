9e9f781ea1089af07825b805986b10e308a40f66
Lennart Boecken <lsboecken@gmail.com>
2023-10-09 20:37:01 -0400

    file rename; custom packer cmd

--- nvim/.config/nvim/lua/lennart/core/cmd.lua
+++ nvim/.config/nvim/lua/lennart/core/cmd.lua
@@ -1,6 +1,12 @@
 vim.api.nvim_create_user_command('Ps', function()
     local date = os.date("%d%m%y%H%M")
     local log_name = date .. ".json"
-    require('packer').snapshot(log_name)
-    print('Packer Snapshot taken & PackerSync executed')
-end, {nargs = 0})
+    local packer_status, packer = pcall(require, 'packer')
+    if not packer_status then
+        print('Packer not installed')
+        return
+    end
+    packer.snapshot(log_name)
+    print('Packer Snapshot taken')
+    packer.sync()
+end, { nargs = 0 })
