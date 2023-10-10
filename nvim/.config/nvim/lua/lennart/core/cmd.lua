93218b89cc3eaebe1dd6d73593f44def1aba0da7
Lennart Boecken <lsboecken@gmail.com>
2023-10-09 20:38:16 -0400

    minor refactor

--- nvim/.config/nvim/lua/lennart/core/cmd.lua
+++ nvim/.config/nvim/lua/lennart/core/cmd.lua
@@ -1,11 +1,12 @@
 vim.api.nvim_create_user_command('Ps', function()
-    local date = os.date("%d%m%y%H%M")
-    local log_name = date .. ".json"
     local packer_status, packer = pcall(require, 'packer')
     if not packer_status then
         print('Packer not installed')
         return
     end
+    
+    local date = os.date("%d%m%y%H%M")
+    local log_name = date .. ".json"
     packer.snapshot(log_name)
     print('Packer Snapshot taken')
     packer.sync()
